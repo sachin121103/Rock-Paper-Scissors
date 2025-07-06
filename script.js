c07818fd083ddf9785ea876b3ccc44400ff1aba5
Sachin Prabhu Ram <sachin.prabhu.ram12@gmail.com>
2025-07-06 18:01:09 +0800

    Built 'GetComputerChoice'

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello, World!")
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice() {
+    choices = ['Rock', 'Paper', 'Scissors']
+    return choices[getRandomInt(3)]
+}
+
